0138e215bac2df2a9b8344cdb18ba4db97f1c9ab
Pranav Gowrishankar <133190114+pranavgowrish@users.noreply.github.com>
2025-04-24 20:59:42 -0700

    Update LandingPage.js

--- frontend/src/LandingPage.js
+++ frontend/src/LandingPage.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NavigationBar from "./NavigationBar"
 import styles from "./LandingPage.module.css";
 import PWAInstallPrompt from "./pwa";
+import iOSInstallPopup from "./iOSInstallPopup";
 
 function LandingPage() {
     return (
@@ -9,6 +10,7 @@ function LandingPage() {
             <NavigationBar />
             <div>
                 <PWAInstallPrompt />
+                <iOSInstallPopup />
             </div>
             <div className={styles.pRow}>
             </div>
