50b8132e9eec307d0223382d74eca5b82b123302
Pranav Gowrishankar <pranavgowrish@gmail.com>
2025-04-27 19:10:10 -0700

    merge success

--- frontend/src/Canvas.js
+++ frontend/src/Canvas.js
@@ -2383,11 +2383,7 @@ const handleIconTouchStart = (e) => {
           //left: `${floatingIconPosition.x}px`,
           left: `${Math.max(
             floatingIconPosition.x,
-<<<<<<< HEAD
             window.innerWidth / 2 + 430
-=======
-            window.innerWidth / 2 + 430
->>>>>>> ac4ceb4b25f055df59c1f9e6a1152866396f3ff6
           )}px`, // Ensure it stays in the right half
           top: `${floatingIconPosition.y}px`,
           backgroundColor: '#98a1f5',
